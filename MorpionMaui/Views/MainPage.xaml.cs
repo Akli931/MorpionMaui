90db3d70c6bf423e2c561dd93dbe9231f10f09e3
Akli931 <aklihadef2001@gmail.com>
2026-03-10 16:31:31 +0100

    Structure de projet en MVVM

---
+++ MorpionMaui/Views/MainPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using MorpionMaui.ViewModels;
+
+namespace MorpionMaui;
+
+public partial class MainPage : ContentPage
+{
+    public MainPage()
+    {
+        InitializeComponent();
+        BindingContext = new GameViewModel();
+    }
+}
